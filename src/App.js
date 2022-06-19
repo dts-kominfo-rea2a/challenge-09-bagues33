2e0ec5b0ea9065b2ff500e2b5bacca79397f6f52
bagues33 <bagustofan@yahoo.com>
2022-06-19 23:50:17 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
+import Contact from './components/Contact.js';
+import Header from './components/Header.js';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Header />
+      <div className="card">
+        <Contact img={contacts[0].photo} alt={contacts[0].name} nama={contacts[0].name} nomor={contacts[0].phone} email={contacts[0].email}/>
+        <Contact img={contacts[1].photo} alt={contacts[1].name} nama={contacts[1].name} nomor={contacts[1].phone} email={contacts[1].email}/>
+        <Contact img={contacts[2].photo} alt={contacts[2].name} nama={contacts[2].name} nomor={contacts[2].phone} email={contacts[2].email}/>
+        <Contact img={contacts[3].photo} alt={contacts[3].name} nama={contacts[3].name} nomor={contacts[3].phone} email={contacts[3].email}/>
+        <Contact img={contacts[4].photo} alt={contacts[4].name} nama={contacts[4].name} nomor={contacts[4].phone} email={contacts[4].email}/>
+        <Contact img={contacts[5].photo} alt={contacts[5].name} nama={contacts[5].name} nomor={contacts[5].phone} email={contacts[5].email}/>
+      </div>
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,20 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css'
 
-const Contact = () => {
+const Contact = (props) => {
     return (
-        <></>
+
+            <div className="card-item">
+                <img src={props.img} alt={props.alt}></img>
+                <div className="card-text">
+                    <h3>{props.nama}</h3>
+                    <p>{props.nomor}</p>
+                    <p>{props.email}</p>
+                </div>
+
+            </div>
     )
 }
 
